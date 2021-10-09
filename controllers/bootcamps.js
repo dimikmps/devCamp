592af6f80d8507fd1cf2d8c80451ca57f6e4c683
dimikmps <dimitris.kmps@gmail.com>
2021-10-09 18:59:54 +0300

    Add routing controller

---
+++ controllers/bootcamps.js
@@ -0,0 +1,64 @@
+// @desc    GET all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({
+    data: {
+      operation: 'GET all bootcamps',
+    },
+    author: 'dkampas',
+    success: 'true',
+  });
+};
+
+// @desc    GET bootcamp by ID
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({
+    data: {
+      operation: `GET bootcamp ${req.params.id}`,
+    },
+    author: 'dkampas',
+    success: 'true',
+  });
+};
+
+// @desc    ADD bootcamp
+// @route   POST /api/v1/bootcamps/
+// @access  Private
+exports.addBootcamp = (req, res, next) => {
+  res.status(200).json({
+    data: {
+      operation: 'ADD new bootcamp',
+    },
+    author: 'dkampas',
+    success: 'true',
+  });
+};
+
+// @desc    UPDATE bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({
+    data: {
+      operation: `UPDATE bootcamp ${req.params.id}`,
+    },
+    author: 'dkampas',
+    success: 'true',
+  });
+};
+
+// @desc    DELETE bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({
+    data: {
+      operation: `DELETE bootcamp ${req.params.id}`,
+    },
+    author: 'dkampas',
+    success: 'true',
+  });
+};
--- routes/router.js
+++ routes/router.js
@@ -1,59 +1,14 @@
 const express = require('express');
-const router = express.Router();  
+const { getBootcamps, getBootcamp, addBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps');
+const router = express.Router();
 
-// GET
-router.get('/', (req, res) => {
-  res.status(200).json({
-    data: {
-      operation: 'GET all bootcamps',
-    },
-    author: 'dkampas',
-    success: 'true',
-  });
-});
+router.route('/')
+    .get(getBootcamps)
+    .post(addBootcamp);
 
-//GET by ID
-router.get('/:id', (req, res) => {
-  res.status(200).json({
-    data: {
-      operation: `GET bootcamp ${req.params.id}`,
-    },
-    author: 'dkampas',
-    success: 'true',
-  });
-});
-
-// POST
-router.post('/', (req, res) => {
-  res.status(200).json({
-    data: {
-      operation: 'ADD new bootcamp',
-    },
-    author: 'dkampas',
-    success: 'true',
-  });
-});
-
-// PUT
-router.put('/:id', (req, res) => {
-  res.status(200).json({
-    data: {
-      operation: `UPDATE bootcamp ${req.params.id}`,
-    },
-    author: 'dkampas',
-    success: 'true',
-  });
-});
-
-// DELETE
-router.delete('/:id', (req, res) => {
-  res.status(200).json({
-    data: {
-      operation: `DELETE bootcamp ${req.params.id}`,
-    },
-    author: 'dkampas',
-    success: 'true',
-  });
-});
+router.route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
 
 module.exports = router;
